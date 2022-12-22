c23a1faecdcfc93fe8aea6ce4d8bdd69f60b4cdd
dannybizman <it.dannybizman@gmail.com>
2022-12-22 03:46:03 +0100

    latest 0.1

---
+++ build/precache-manifest.d38233702ef588b376502884ff0ffac3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec13cfa72f3360268a0f938de19fbc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0254b28d11d48f91fd7b",
+    "url": "/static/css/main.2bd5ab95.chunk.css"
+  },
+  {
+    "revision": "41a694feda5162aef015",
+    "url": "/static/js/2.50f4bdb5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.50f4bdb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0254b28d11d48f91fd7b",
+    "url": "/static/js/main.27aa8101.chunk.js"
+  },
+  {
+    "revision": "b026e7bb2026f93a035a",
+    "url": "/static/js/runtime-main.681520df.js"
+  }
+]);
